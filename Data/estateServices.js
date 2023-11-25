6b5889739d46a8ef3e5fde0736438448c9e7eae2
Emma Udeji <gogreneinfo@gmail.com>
2023-11-25 05:22:45 +0100

    cleaning up the code

--- Data/estateServices.js
+++ Data/estateServices.js
@@ -4,25 +4,25 @@ import {MdOutlineGppGood} from "react-icons/md"
 export const estateServices = [
     {
         id: 0,
-        title: `Buy aHome`,
+        title: `Buy a home`,
         text: `Unlock the door to your dream home with our personalized buying solutions.`,
         icon: <MdOutlineGppGood/>
     }, 
     {
         id: 1,
-        title: `Rent aHome`,
+        title: `Rent a home`,
         text: `From budget-friendly to luxurious, we have the perfect rental home for you.`,
         icon: <MdOutlineGppGood/>
     },
     {
         id: 2,
-        title: `Sell aHome`,
+        title: `Sell a home`,
         text: `From staging to closing, we're with you every step of the way to help you sell a home.`,
         icon: <MdOutlineGppGood/>
     },
     {
         id: 3,
-        title: `Evaluate a Home`,
+        title: `Evaluate a home`,
         text: `Make informed decisions with our thorough and detailed home evaluation support`,
         icon: <MdOutlineGppGood/>
     },
